01b77c6b606f46ab99c9787e1be44e0aebf634d6
Sean Hoar <sean.hoar@gmail.com>
2026-03-02 21:08:15 -0500

    feat/ markdown-based faq

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
     post "/weddings/:wedding_id/ask", to: "ask#create"
     get "/weddings/:wedding_id/ask/stream", to: "ask_stream#stream"
     get "/weddings/:wedding_id/guestbook_entries", to: "guestbook_entries#index"
+
+    get "docs", to: "docs#index"
+    get "docs/:filename", to: "docs#show", constraints: { filename: /[^\/]+/ }
   end
 end
